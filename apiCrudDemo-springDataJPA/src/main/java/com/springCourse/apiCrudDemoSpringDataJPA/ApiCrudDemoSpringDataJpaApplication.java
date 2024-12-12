8a1660b60089dc6543074e2ab68e1f7c9b62c6eb
Rubick <eithanllanosb@gmail.com>
2024-12-12 08:15:33 -0500

    Set up src project and SQLscript base implementation

---
+++ apiCrudDemo-springDataJPA/src/main/java/com/springCourse/apiCrudDemoSpringDataJPA/ApiCrudDemoSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springCourse.apiCrudDemoSpringDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudDemoSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudDemoSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ apiCrudDemo-springDataJPA/src/test/java/com/springCourse/apiCrudDemoSpringDataJPA/ApiCrudDemoSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springCourse.apiCrudDemoSpringDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudDemoSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
