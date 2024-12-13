0fecd480296db7caacdb1f3b1088a7340cfc3099
Rubick <eithanllanosb@gmail.com>
2024-12-13 09:01:32 -0500

    Implement springBoot Data rest for more efficient code

--- apiCrudDemo-springDataJPA/src/main/java/com/springCourse/apiCrudDemoSpringDataJPA/dao/EmployeeRepository.java
+++ apiCrudDemo-springDataJPA/src/main/java/com/springCourse/apiCrudDemoSpringDataJPA/dao/EmployeeRepository.java
@@ -3,7 +3,8 @@ package com.springCourse.apiCrudDemoSpringDataJPA.dao;
 import com.springCourse.apiCrudDemoSpringDataJPA.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    // and thats it
+    // and thats it!
 }
