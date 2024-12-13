eb5ba885fb3a9ae35136006f84b91bcab23bb725
Rubick <eithanllanosb@gmail.com>
2024-12-13 09:06:06 -0500

    Added Custom properties to SpringBoot Data Rest

--- apiCrudDemo-springDataJPA/src/main/java/com/springCourse/apiCrudDemoSpringDataJPA/ApiCrudDemoSpringDataJpaApplication.java
+++ apiCrudDemo-springDataJPA/src/main/java/com/springCourse/apiCrudDemoSpringDataJPA/ApiCrudDemoSpringDataJpaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiCrudDemoSpringDataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiCrudDemoSpringDataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCrudDemoSpringDataJpaApplication.class, args);
+    }
 
 }
--- apiCrudDemo-springDataJPA/src/main/java/com/springCourse/apiCrudDemoSpringDataJPA/dao/EmployeeRepository.java
+++ apiCrudDemo-springDataJPA/src/main/java/com/springCourse/apiCrudDemoSpringDataJPA/dao/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.springCourse.apiCrudDemoSpringDataJPA.dao;
 
 import com.springCourse.apiCrudDemoSpringDataJPA.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+//add custom path, if not specified it uses a plural, lowercase version of the entity name.
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // and thats it!
